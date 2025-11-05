1c5228d730e6e3a673f7afe941833ff478e370aa
knowledgeless <nazmulhossainxi9@gmail.com>
2025-11-05 09:46:54 -0800

    career path uploaded

--- app/views.py
+++ app/views.py
@@ -1,9 +1,45 @@
 from django.shortcuts import render
 
-# Create your views here.
-def home(request):
-    return render(request, "html/home.html")
 
+
+
+def home(request):
+    services = ["Web Development", "Django Backend", "Python Automation", "Web Scraping", "Web Design", "Digital Poster Design", "Cover Page Design", "Ad Design", "Print Media Design"]
+
+    career_steps = [
+        {
+            "year": "2025",
+            "title": "Graphics Designer",
+            "description": "Bangladesh Open Source Network (BdOSN).",
+        },
+        {
+            "year": "2023",
+            "title": "Graphics Designer Intern",
+            "description": "Intellic IT LLC.",
+        },
+        {
+            "year": "2022",
+            "title": "Design Advisor",
+            "description": "Southeast Computer Club, Southeast University",
+        },
+        {
+            "year": "2021",
+            "title": "Graphics Design Team Lead",
+            "description": "Southeast Computer Club, Southeast University.",
+        },
+        {
+            "year": "2020",
+            "title": "Graphics Designer",
+            "description": "Southeast Computer Club, Southeast University.",
+        },
+        {
+            "year": "2018",
+            "title": "Junior Graphics Designer",
+            "description": "Line Work, Panthapath, Dhaka",
+        },
+    ]
+
+    return render(request, "html/home.html", {"services": services, "career_steps": career_steps})
 
 def graphics(request):
     return render(request, "html/graphics.html")
